1670b972127a4d90787666a856d39aa556084a65
winterrdog <winterrdog@keemail.me>
2022-07-24 13:07:42 +0300

    fixed text format

--- install_win.ps1
+++ install_win.ps1
@@ -4,5 +4,5 @@ Move-Item -Path ./target/release/img_combiner -Destination .
 Write-Output "`n[+] Removing artifacts...`n"
 cargo clean
 
-printf "`n[+] Now, you can run img_combiner like this`n"
+printf "`n[+] Now, you can run img_combiner like this:`n"
 printf "`t./img_combiner <filepath_of_first_image> <filepath_of_second_image> <filepath_of_output_image> `n"
