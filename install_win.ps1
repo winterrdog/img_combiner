197bfcda7c9452460e21bcd88d09002bc01ed98c
winterrdog <winterrdog@keemail.me>
2022-07-24 12:55:39 +0300

    added `install_win.ps1` for Windows installation

---
+++ install_win.ps1
@@ -0,0 +1,8 @@
+cargo build --release
+Move-Item -Path ./target/release/img_combiner -Destination .
+
+Write-Output "`n[+] Removing artifacts...`n"
+cargo clean
+
+printf "`n[+] Now, you can run img_combiner like this`n"
+printf "`t./img_combiner <filepath_of_first_image> <filepath_of_second_image> <filepath_of_output_image> `n"
